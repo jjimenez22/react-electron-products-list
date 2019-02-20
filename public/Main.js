d60e8a2e4866c3992333d4849cb649e49e900d75
Jesus Jimenez <jgjimenezb94@gmail.com>
2019-02-19 22:28:42 -0400

    Primer commit

---
+++ public/Main.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const {app, BrowserWindow} = require('electron');
+
+function createWindow() {
+    // Create the browser window.
+    let win = new BrowserWindow({width: 800, height: 600});
+
+// and load the index.html of the app.
+    win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`)
+    // win.webContents.openDevTools();//enable browser development tools
+}
+
+app.on('ready', createWindow);
